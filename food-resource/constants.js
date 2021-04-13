78ce541e5eb8b30836e80861f419abda7de93ffb
meganrm <meganrm@gmail.com>
2021-04-12 17:58:35 -0700

    fixed food bank columns

--- food-resource/constants.js
+++ food-resource/constants.js
@@ -1,22 +1,23 @@
 const HEADERS = [
-    "title",
-    "fridge",
-    "freezer",
-    "pantry",
-    "foodBank",
-    "state",
-    "city",
-    "address",
-    "hours",
-    "organization",
-    "contact",
-    "twitter",
-    "instagram",
-    "facebook",
-    "website",
-    "phone",
-    "notes",
-    "delete",
+    "title", //a
+    "fridge", //b
+    "freezer", //c
+    "pantry", //d
+    "foodBank", //e
+    "state", //f
+    "city", //g
+    "address", //h
+    "hours", //i
+    "organization", //j
+    "contact", //k
+    "twitter", //l
+    "instagram", //m
+    "funding", //n
+    "facebook", //o
+    "website", //p
+    "phone", //q
+    "notes", //r
+    "delete", //s
 ]
 
 module.exports = HEADERS;
